782f7956ff1da67278f27c9fc65a277452b7676e
thanhnd <ducthanhnguyen95@gmail.com>
2023-03-25 01:23:35 +0700

    Using dependency injection with abstractions (Deciding which objects should be part of the Spring context, DI do not use stereotype annotation (@Component...))

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,28 +1,23 @@
 package services;
 
 import model.Comment;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
 
-@Component
 public class CommentService {
 
-    @Autowired
     private CommentRepository commentRepository;
 
-    @Autowired
     private CommentNotificationProxy commentNotificationProxy;
 
 //    /*
 //    We would have to use @Autowired if the class had more than one constructor.
 //    @Autowired
 //     */
-//    public CommentService (CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
-//        this.commentRepository = commentRepository;
-//        this.commentNotificationProxy = commentNotificationProxy;
-//    }
+    public CommentService (CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
 
     public void publishComment(Comment comment) {
         commentRepository.storeComment(comment);
