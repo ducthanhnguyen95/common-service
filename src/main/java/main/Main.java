733840ee57838ac7b3758d78a72d8b697ff144cc
thanhnd <ducthanhnguyen95@gmail.com>
2023-03-25 01:11:14 +0700

    Using dependency injection with abstractions (Deciding which objects should be part of the Spring context, DI via constructor)

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,19 +1,18 @@
 package main;
 
+import configuration.ProjectConfiguration;
 import model.Comment;
-import proxies.EmailCommentNotificationProxy;
-import repositories.DBCommentRepository;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import services.CommentService;
 
 public class Main {
 
     public static void main(String[] args) {
-        var commentRepository = new DBCommentRepository();
-        var commentNotificationProxy = new EmailCommentNotificationProxy();
-        var commentService = new CommentService(commentRepository, commentNotificationProxy);
+        var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
         var comment = new Comment();
         comment.setAuthor("Laurentiu");
         comment.setText("Demo comment");
+        var commentService = context.getBean(CommentService.class);
         commentService.publishComment(comment);
     }
 }
--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,15 +1,21 @@
 package services;
 
 import model.Comment;
+import org.springframework.stereotype.Component;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
 
+@Component
 public class CommentService {
 
     private final CommentRepository commentRepository;
 
     private final CommentNotificationProxy commentNotificationProxy;
 
+    /*
+    We would have to use @Autowired if the class had more than one constructor.
+    @Autowired
+     */
     public CommentService (CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
