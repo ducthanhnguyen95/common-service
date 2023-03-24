74d8c70ce79d14017a9489559fd4afdf05e97515
thanhnd <ducthanhnguyen95@gmail.com>
2023-03-25 01:31:51 +0700

    Using dependency injection with abstractions (Deciding which objects should be part of the Spring context, DI via setter)

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,23 +1,38 @@
 package services;
 
 import model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
 
+@Component
 public class CommentService {
 
-    private CommentRepository commentRepository;
+    private  CommentRepository commentRepository;
 
-    private CommentNotificationProxy commentNotificationProxy;
+    private  CommentNotificationProxy commentNotificationProxy;
 
-//    /*
+    @Autowired
+    public void setCommentRepository(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Autowired
+    public void setCommentNotificationProxy(CommentNotificationProxy commentNotificationProxy) {
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    //    /*
 //    We would have to use @Autowired if the class had more than one constructor.
 //    @Autowired
 //     */
-    public CommentService (CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
-        this.commentRepository = commentRepository;
-        this.commentNotificationProxy = commentNotificationProxy;
-    }
+//    public CommentService (CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+//        this.commentRepository = commentRepository;
+//        this.commentNotificationProxy = commentNotificationProxy;
+//    }
+
+
 
     public void publishComment(Comment comment) {
         commentRepository.storeComment(comment);
